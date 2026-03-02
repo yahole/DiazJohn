ca865be7d1985aa305871001c6dabaa7caf3ae20
John Michael <111406110+yahole@users.noreply.github.com>
2026-03-02 10:33:11 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,3 +25,14 @@ function navHighlighter() {
       }
     });
   }
+
+const glow = document.getElementById('mouse-glow');
+
+window.addEventListener('mousemove', (e) => {
+    // We subtract half the width/height (200px) to center the glow on the cursor
+    const x = e.clientX - 200;
+    const y = e.clientY - 200;
+    
+    glow.style.setProperty('--x', `${x}px`);
+    glow.style.setProperty('--y', `${y}px`);
+});
